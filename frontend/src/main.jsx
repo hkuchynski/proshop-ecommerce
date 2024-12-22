d52dcfa5518e7c39f52f55e378624c3d1508e536
Hanna Kuchynski <hannakuchynski@Hannas-MBP.fios-router.home>
2024-12-22 12:32:14 -0500

    added shopping cart component and shopping cart functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,7 @@ import store from '../src/store.js';
 import App from './App.jsx';
 import HomeScreen from './screens/HomeScreen.jsx';
 import ProductScreen from './screens/ProductScreen.jsx';
+import CartScreen from './screens/CartScreen.jsx';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 );
